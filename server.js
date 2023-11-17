31b6748b256e5b980fbf8c9e657b1165a5d7a11b
wowDongHyeon <rnel8036@naver.com>
2023-11-17 14:20:17 +0900

    env추가

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
-
+import dotenv from 'dotenv';
 
 //TODO role: "assistant"역할 부여도 파라미터에 넣어야함
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors()); // CORS 미들웨어 추가
 
+// const configuration = new Configuration({
+//   organization: "org-qDHKBQQkfeOwrWiL0lEkbcGq",
+//   apiKey: "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",
+// });
+
 const configuration = new Configuration({
-  organization: "org-qDHKBQQkfeOwrWiL0lEkbcGq",
-  apiKey: "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",
+  organization: process.env.OPENAI_ORGANIZATION,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -24,6 +30,7 @@ app.post('/chat', async (req, res) => {
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: req.body.message }],
+      // messages: [{ role: "user", content: "안녕" }],
     });
     console.log('시작후');
     res.json(response.data.choices[0].message.content);
